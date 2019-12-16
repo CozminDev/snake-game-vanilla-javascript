07c835638e96de7f282cb7b49f2ee115e1128723
Cozmin <cozmindeveloper@yahoo.com>
2019-12-16 12:03:31 +0200

    Added food and endgame logic

--- main.js
+++ main.js
@@ -9,24 +9,49 @@ let snake = [
 let dx = 10;
 let dy = 0;
 
+let isMovingRight = true;
+let isMovingLeft = false;
+let isMovingUp = false;
+let isMovingDown = false;
+
+let food = {};
+
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
 
+//Start
+main();
+document.addEventListener("keydown", e => changeDirection(e));
+
 function main() {
-  setInterval(function onTick() {
+  generateFood();
+  let interval = setInterval(function onTick() {
     clearCanvas();
+    drawFood();
     drawSnake();
     advanceSnake();
+    if (didGameEnd()) {
+      clearInterval(interval);
+      let elem = document.getElementById("game-title");
+      elem.innerHTML = "Game Over";
+    }
   }, 100);
 }
 
-function randomTen(min, max){
-    return Math.round((Math.random() * (max-min) + min) / 10 ) * 10;
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawFood() {
+  ctx.fillStyle = "#FF0000";
+  ctx.strokeStyle = "#000000";
+
+  ctx.fillRect(food.x, food.y, 10, 10);
+  ctx.strokeRect(food.x, food.y, 10, 10);
 }
 
 function drawSnake() {
   snake.forEach(snakePart => drawSnakePart(snakePart));
-  console.log(randomTen(0, canvas.width));
 }
 
 function drawSnakePart(snakePart) {
@@ -37,22 +62,51 @@ function drawSnakePart(snakePart) {
 }
 
 function advanceSnake() {
+  checkCurrentDirection();
   let head = {};
-  if (snake[0].x == canvas.width - 10 && snake[2].x == snake[0].x - 20) {
+  if (snake[0].x == canvas.width - 10 && isMovingRight) {
     head = { x: 0, y: snake[0].y };
-  } 
-  else if (snake[0].x == 0 && snake[2].x == snake[0].x + 20) {
+  } else if (snake[0].x == 0 && isMovingLeft) {
     head = { x: canvas.width, y: snake[0].y };
-  }
-  else if (snake[0].y == canvas.height - 10 && snake[2].y == snake[0].y - 20) {
+  } else if (snake[0].y == canvas.height - 10 && isMovingUp) {
     head = { x: snake[0].x, y: 0 };
-  }
-  else if (snake[0].y == 0 && snake[2].y == snake[0].y + 20) {
+  } else if (snake[0].y == 0 && isMovingDown) {
     head = { x: snake[0].x, y: canvas.height };
-  }
-  else head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  } else head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
-  snake.pop();
+
+  const didEatFood = snake[0].x == food.x && snake[0].y == food.y;
+  if (didEatFood) {
+    generateFood();
+  } else {
+    snake.pop();
+  }
+}
+
+function checkCurrentDirection() {
+  isMovingRight = dx == 10;
+  isMovingLeft = dx == -10;
+  isMovingUp = dy == 10;
+  isMovingDown = dy == -10;
+}
+
+function generateFood() {
+  food = {
+    x: randomTen(0, canvas.width - 10),
+    y: randomTen(0, canvas.height - 10)
+  };
+}
+
+function randomTen(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+function didGameEnd() {
+  for (let index = 4; index < snake.length; index++) {
+    if (snake[0].x == snake[index].x && snake[0].y == snake[index].y)
+      return true;
+  }
+  return false;
 }
 
 function changeDirection(e) {
@@ -62,10 +116,7 @@ function changeDirection(e) {
   const KEY_UP = 38;
   const KEY_DOWN = 40;
 
-  const isMovingRight = dx == 10;
-  const isMovingLeft = dx == -10;
-  const isMovingUp = dy == 10;
-  const isMovingDown = dy == -10;
+  checkCurrentDirection();
 
   if (keyCode == KEY_RIGHT && (isMovingUp || isMovingDown)) {
     dx = 10;
@@ -87,11 +138,3 @@ function changeDirection(e) {
     dy = 10;
   }
 }
-
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-main();
-
-document.addEventListener("keydown", e => changeDirection(e));
